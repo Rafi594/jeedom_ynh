b5fd967ba0b50786f77be4d1c004b29babba429f
lunarok <lunarok@gmail.com>
2014-08-10 00:05:01 +0200

    Update ldap_config.sql

--- conf/ldap_config.sql
+++ conf/ldap_config.sql
@@ -1,7 +1,7 @@
 INSERT INTO config (plugin, key, value) VALUES
 ('core', 'ldap:basedn', 'ou=users,dc=yunohost,dc=org'),
-('core', 'ldap:host', 'localhost'),
 ('core', 'ldap:domain', 'dc=yunohost,dc=org'),
 ('core', 'ldap:enable', '1'),
 ('core', 'ldap:filter', 'objectClass=mailAccount'),
+('core', 'ldap:host', 'localhost'),
 ('core', 'ldap:port', '389');
