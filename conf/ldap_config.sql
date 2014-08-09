3357538913aff2bf9b1c11fffb8124e1475f25ce
lunarok <lunarok@gmail.com>
2014-08-09 22:51:54 +0200

    Create ldap_config.sql

---
+++ conf/ldap_config.sql
@@ -0,0 +1,7 @@
+REPLACE INTO config (plugin, key, value) VALUES
+('core', 'ldap:basedn', 'ou=users,dc=yunohost,dc=org'),
+('core', 'ldap:host', 'localhost'),
+('core', 'ldap:domain', 'dc=yunohost,dc=org'),
+('core', 'ldap:enable', '1'),
+('core', 'ldap:filter', 'objectClass=mailAccount'),
+('core', 'ldap:port', '389');
