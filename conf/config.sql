df1b95e640a0bb5ab4ae3875c0f9d8110cb082ac
scith <scith@hotmail.fr>
2016-07-25 17:10:49 +0200

    OpenZwave, trap and config

---
+++ conf/config.sql
@@ -0,0 +1,20 @@
+INSERT INTO config (`plugin`, `key`, `value`) VALUES 
+('core', 'ldap:basedn', 'ou=users,dc=yunohost,dc=org'),
+('core', 'ldap:domain', 'dc=yunohost,dc=org'),
+('core', 'ldap:enable', '1'),
+('core', 'ldap:filter', 'objectClass=mailAccount'),
+('core', 'ldap:host', 'localhost'),
+('core', 'ldap:port', '389'),
+('core', 'sso:allowRemoteUser', '1'),
+('core', 'internalAddr', 'YNH_DOMAIN'),
+('core', 'internalComplement', 'YNH_PATH'),
+('core', 'internalPort', ''),
+('core', 'internalProtocol', 'https://'),
+('core', 'externalAddr', 'YNH_DOMAIN'),
+('core', 'externalComplement', 'YNH_PATH'),
+('core', 'externalPort', ''),
+('core', 'externalProtocol', 'https://');
+
+INSERT INTO user (id, login, password, options, hash, rights) VALUES ('0', 'YNH_ADMIN', '', NULL, NULL, '{\"admin\":1}');
+
+DELETE FROM user WHERE login = 'admin';
